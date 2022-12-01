ca525a7d1bfde89153ee53980d6e8ab864fc25e4
Ryan Rustyn <ryanrustyn@gmail.com>
2022-11-30 21:47:46 -0800

    O(n) solution

--- LeetCode/Two-Sum/JS/twosum.js
+++ LeetCode/Two-Sum/JS/twosum.js
@@ -9,15 +9,19 @@
 // sumToTarget([2,5,8], 10) -> [0,2]
 
 function sumToTarget(nums, targetSum) {
-  for (let i = 0; i < nums.length; i++) {
-    debugger
-    for (let j = i+1; i < nums.length; j++) {
-      debugger
-      if (nums[i] + nums[j] === targetSum) {
-        return [i,j];
-      }
-    }
-  }
+  //O(n**2) solution
+  //
+  // for (let i = 0; i < nums.length; i++) {
+    
+  //   for (let j = i+1; i < nums.length; j++) {
+      
+  //     if (nums[i] + nums[j] === targetSum) {
+  //       return [i,j];
+  //     }
+  //   }
+  // }
 }
+
+
 
 module.exports = {sumToTarget}
