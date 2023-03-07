da0f860c1f7e69f73450dcf679b024b261d82950
Ryan Rustyn <ryanrustyn@gmail.com>
2023-03-07 08:49:28 -0800

    fixed sorted not being referenced

--- mergeintervals.js
+++ mergeintervals.js
@@ -4,24 +4,24 @@
  */
 var merge = function(intervals) {
     
-    let sortedIntervals = intervals.sort((a,b) => a[0] - b[0])
+  let sortedIntervals = intervals.sort((a,b) => a[0] - b[0]);
 
-    let newIntervals = [sortedIntervals[0]]
-    let newIndex = 0;
+  let newIntervals = [sortedIntervals[0]];
+  let newIndex = 0;
 
-    for (let i = 1; i < intervals.length; i++) {
-        let next = intervals[i];
+  for (let i = 1; i < sortedIntervals.length; i++) {
+      let next = sortedIntervals[i];
 
-        if (newIntervals[newIndex][1] >= next[0]) {
-            newIntervals[newIndex] = [
-                Math.min(newIntervals[newIndex][0],next[0]),
-                Math.max(newIntervals[newIndex][1],next[1])
-                ]
-        } else {
-            newIntervals.push(next);
-            newIndex++;
-        }
-    }
+      if (newIntervals[newIndex][1] >= next[0]) {
+          newIntervals[newIndex] = [
+              Math.min(newIntervals[newIndex][0],next[0]),
+              Math.max(newIntervals[newIndex][1],next[1])
+              ]
+      } else {
+          newIntervals.push(next);
+          newIndex++;
+      }
+  }
 
-    return newIntervals;
+  return newIntervals;
 };
